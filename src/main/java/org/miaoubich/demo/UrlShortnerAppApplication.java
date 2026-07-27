5673f92f03edf22be5586c924b2d5a4d588a67ed
Ali Bouzar <ali.bouzar@gmail.com>
2026-07-27 16:45:35 +0200

    Initial Sprint Boot 4.1.0 project setup

---
+++ src/main/java/org/miaoubich/demo/UrlShortnerAppApplication.java
@@ -0,0 +1,13 @@
+package org.miaoubich.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortnerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortnerAppApplication.class, args);
+	}
+
+}
